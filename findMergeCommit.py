05bb3af80628e3dc2a15f348f5945bae3f6413e7
hiufungk <hiufungk@ualberta.ca>
2019-03-28 14:18:09 -0600

    added function to get pr info from github api

--- findMergeCommit.py
+++ findMergeCommit.py
@@ -1,5 +1,7 @@
 from pydriller import RepositoryMining
 import sqlite3
+import requests
+import json
 
 ''' 
 searchSHA: String of the commit hash that you are looking for (this should be a PR)
@@ -8,25 +10,46 @@ testList: Array of testfiles that have been modified in this PR
 repositoryLink: The https url to the git repository you are mining
 '''
 
-searchSHA = 'b7b1c7f11b70d4758a5fe86f507a10e1b8c982eb'
-mergeCommit = ''
-checkMergeFlag = False
-testList = []
-repositoryLink = "https://github.com/AChep/AcDisplay.git"
+# searchSHA = 'b7b1c7f11b70d4758a5fe86f507a10e1b8c982eb'
+# mergeCommit = ''
+# checkMergeFlag = False
+# testList = []
+# repositoryLink = "https://github.com/AChep/AcDisplay.git"
 
-for commit in RepositoryMining(repositoryLink).traverse_commits():
-    if (searchSHA in commit.hash):
-        checkMergeFlag = True
+# for commit in RepositoryMining(repositoryLink).traverse_commits():
+#     if (searchSHA in commit.hash):
+#         checkMergeFlag = True
 
-    if checkMergeFlag is True:
-        if commit.merge is True:
-            mergeCommit = commit.hash
-            for modified_file in commit.modifications:
-                if ("Test" in modified_file.filename or "test" in modified_file.filename):
-                    testList.append(modified_file.filename)
-            break
+#     if checkMergeFlag is True:
+#         if commit.merge is True:
+#             mergeCommit = commit.hash
+#             for modified_file in commit.modifications:
+#                 if ("Test" in modified_file.filename or "test" in modified_file.filename):
+#                     testList.append(modified_file.filename)
+#             break
         
-print("The corresponding merge commit hash is " + str(mergeCommit) + " with these possible tests " + str(testList))
+# print("The corresponding merge commit hash is " + str(mergeCommit) + " with these possible tests " + str(testList))
+
+def get_pull_request_info(repo, pr_num):
+    output = {}
+    pr_endpoint = "https://api.github.com/repos/{}/pulls/{}/commits".format(repo, pr_num)
+    response = requests.get(pr_endpoint)
+    json_response = response.json()
+    # commit before this pr branch off the original branch
+    output["parent_commit"] = json_response[0]["parents"][0]["sha"]
+    output["oldest_commit_in_pr"] = json_response[0]["sha"]
+    output["latestest_commit_in_pr"] = json_response[-1]["sha"]
+
+    files_changed_endpoint = "https://api.github.com/repos/{}/pulls/{}/files".format(repo, pr_num)
+    files_changed = []
+    response = requests.get(files_changed_endpoint)
+    for file_obj in response.json():
+        # does status: added, modified, deleted matter?
+        files_changed.append(file_obj["filename"])
+    
+    output["files_changed"] = files_changed
+    return output
+
 
 def get_repo_names(cur):
     query = """select distinct  gh_project_name from travis where (gh_is_pr == "TRUE") LIMIT 30;"""
@@ -34,13 +57,20 @@ def get_repo_names(cur):
     repos = []
     for row in results:
         repos.append(row[0])
+        print(row[0])
     return repos
 
 def main():
     con = sqlite3.connect('travis.db')  
     cur = con.cursor()
     repo_names = get_repo_names(cur)
-    # print(repo_names)
+    for repo_name in repo_names:
+        # output = get_pull_request_info(repo_name, 202)
+        # print(output)
+        # break
+        pass
+
+
     return 
 
 if __name__ == "__main__":
