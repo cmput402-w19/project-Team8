4176f8a6d22930cdf86fe7e1879e9135762701a1
hiufungk <hiufungk@ualberta.ca>
2019-03-27 18:23:58 -0600

    added connection to db

--- findMergeCommit.py
+++ findMergeCommit.py
@@ -1,4 +1,5 @@
 from pydriller import RepositoryMining
+import sqlite3
 
 ''' 
 searchSHA: String of the commit hash that you are looking for (this should be a PR)
@@ -26,3 +27,21 @@ for commit in RepositoryMining(repositoryLink).traverse_commits():
             break
         
 print("The corresponding merge commit hash is " + str(mergeCommit) + " with these possible tests " + str(testList))
+
+def get_repo_names(cur):
+    query = """select distinct  gh_project_name from travis where (gh_is_pr == "TRUE") LIMIT 30;"""
+    results = cur.execute(query)
+    repos = []
+    for row in results:
+        repos.append(row[0])
+    return repos
+
+def main():
+    con = sqlite3.connect('travis.db')  
+    cur = con.cursor()
+    repo_names = get_repo_names(cur)
+    # print(repo_names)
+    return 
+
+if __name__ == "__main__":
+    main()
