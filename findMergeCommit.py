f721236d7e5c8deb1b2c43663a2d7e11d23aaa53
Matthew Chung <Matthew_Chung@intuit.com>
2019-03-28 19:49:02 -0600

    passes in PR and starts printing density comparisons

--- findMergeCommit.py
+++ findMergeCommit.py
@@ -10,25 +10,7 @@ testList: Array of testfiles that have been modified in this PR
 repositoryLink: The https url to the git repository you are mining
 '''
 
-# searchSHA = 'b7b1c7f11b70d4758a5fe86f507a10e1b8c982eb'
-# mergeCommit = ''
-# checkMergeFlag = False
-# testList = []
-# repositoryLink = "https://github.com/AChep/AcDisplay.git"
 
-# for commit in RepositoryMining(repositoryLink).traverse_commits():
-#     if (searchSHA in commit.hash):
-#         checkMergeFlag = True
-
-#     if checkMergeFlag is True:
-#         if commit.merge is True:
-#             mergeCommit = commit.hash
-#             for modified_file in commit.modifications:
-#                 if ("Test" in modified_file.filename or "test" in modified_file.filename):
-#                     testList.append(modified_file.filename)
-#             break
-        
-# print("The corresponding merge commit hash is " + str(mergeCommit) + " with these possible tests " + str(testList))
 
 def get_pull_request_info(repo, pr_num):
     output = {}
@@ -38,7 +20,7 @@ def get_pull_request_info(repo, pr_num):
     # commit before this pr branch off the original branch
     output["parent_commit"] = json_response[0]["parents"][0]["sha"]
     output["oldest_commit_in_pr"] = json_response[0]["sha"]
-    output["latestest_commit_in_pr"] = json_response[-1]["sha"]
+    output["latest_commit_in_pr"] = json_response[-1]["sha"]
 
     files_changed_endpoint = "https://api.github.com/repos/{}/pulls/{}/files".format(repo, pr_num)
     files_changed = []
@@ -50,6 +32,19 @@ def get_pull_request_info(repo, pr_num):
     output["files_changed"] = files_changed
     return output
 
+def test_density_comparison(cur, merge_commit, before_commit, project): 
+    
+    query = """
+                SELECT DISTINCT t1.gh_test_lines_per_kloc as BeforeLineDensity, t2.gh_test_lines_per_kloc as AfterTestDensity 
+                FROM travis t1, travis t2 
+                WHERE t1.git_trigger_commit == ? 
+                AND t2.git_trigger_commit == ? 
+                AND t1.gh_project_name == ?
+                AND t2.gh_project_name == ?;
+                """
+    results = cur.execute(query, (before_commit, merge_commit, project, project))
+    for row in results:
+        print("Project: " + str(project) + " Before Density: " + str(row[0]) + " After Density: " + str(row[1]) + " Difference: " + str(float(row[1]) - float(row[0])))
 
 def get_repo_names(cur):
     query = """select distinct  gh_project_name from travis where (gh_is_pr == "TRUE") LIMIT 30;"""
@@ -57,20 +52,32 @@ def get_repo_names(cur):
     repos = []
     for row in results:
         repos.append(row[0])
-        print(row[0])
+        #print(row[0])
     return repos
+
+def get_prs(cur, project): 
+    query = """select distinct gh_pull_req_num from travis where gh_is_pr == "TRUE" and gh_project_name == ? LIMIT 30;"""
+    results = cur.execute(query, (project,))
+    pr_list = []
+    for row in results:
+        pr_list.append(row[0])
+    return pr_list
 
 def main():
     con = sqlite3.connect('travis.db')  
     cur = con.cursor()
     repo_names = get_repo_names(cur)
     for repo_name in repo_names:
-        # output = get_pull_request_info(repo_name, 202)
-        # print(output)
-        # break
+        repo_prs = get_prs(cur, repo_name)
+        print("Analyzing: " + repo_name)
+        for pr in repo_prs:
+            print("Looking at PR: " + str(pr))
+            pr_num = int(pr)
+            output = get_pull_request_info(repo_name, pr_num)
+            test_density_comparison(cur, output["latest_commit_in_pr"], output["parent_commit"], repo_name)
+        # When we are confident that our oauth is setup we can remove this break. Leaving it so we dont get banned
+        break
         pass
-
-
     return 
 
 if __name__ == "__main__":
